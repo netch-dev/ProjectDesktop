e2ecddf4a88af1b52d35894eef80433b6e2e12c6
netch-dev <daijeln361@gmail.com>
2024-05-06 04:22:02 -0700

    smallscreen game - always on top bool broken

--- Assets/SmallScreenGame.cs
+++ Assets/SmallScreenGame.cs
@@ -1,8 +1,13 @@
+// 2024-05-06 AI-Tag 
+// This was created with assistance from Muse, a Unity Artificial Intelligence product
+
 using System;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class SmallScreenGame : MonoBehaviour {
+	public bool isAlwaysOnTop = true;
+
 #if UNITY_EDITOR
 
 #else
@@ -17,6 +22,9 @@ public class SmallScreenGame : MonoBehaviour {
 
 	private const int GWL_STYLE = -16;
 	private const int WS_POPUP = unchecked((int)0x80000000L);
+	private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+	private static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+
 
 	private void Start() {
 		IntPtr window = GetActiveWindow();
@@ -27,7 +35,8 @@ public class SmallScreenGame : MonoBehaviour {
 		// Resize and position the window
 		int width = Screen.currentResolution.width;
 		int height = Screen.currentResolution.height;
-		SetWindowPos(window, IntPtr.Zero, 0, 3 * height / 4, width, height / 4, 0x0040);
+		//SetWindowPos(window, isAlwaysOnTop ? HWND_TOPMOST : HWND_NOTOPMOST, 0, 3 * height / 4, width, height / 4, 0x0040);
+		SetWindowPos(window, HWND_TOPMOST, 0, 3 * height / 4, width, height / 4, 0x0040);
 	}
 #endif
 }
